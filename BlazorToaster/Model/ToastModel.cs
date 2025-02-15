09efc82cc4241defd1c07a80ddac4af1e868fc94
hayato <umai-beer@hotmail.co.jp>
2025-02-15 17:22:24 +0900

    ok

--- BlazorToaster/Model/ToastModel.cs
+++ BlazorToaster/Model/ToastModel.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace BlazorToaster.Model
 {
-    public class ToastModel:IToastModel
+    public class ToastModel:IToastModel,IDisposable
     {
+        readonly Action _removeAction;
         public Guid Id { get; }=Guid.NewGuid();
 
         public int CloosedTimer { get; } = 3000;
@@ -16,9 +17,30 @@ namespace BlazorToaster.Model
 
         public bool CloseButton { get; set; }
 
-        public ToastModel(int cloosedTimer)
+
+        public ToastModel(Action removeAction,int cloosedTimer)
         {
             CloosedTimer = cloosedTimer;
+            _removeAction = removeAction;
+        }
+
+        public async Task SaartAsync()
+        {
+            if (0 > CloosedTimer)
+            {
+                return;
+            }
+            await Task.Delay(CloosedTimer).ContinueWith(task => 
+            {
+                if (task.Status == TaskStatus.Canceled)
+                {
+                    Dispose();
+                }
+            });
+        }
+        public void Dispose()
+        {
+            _removeAction.Invoke();
         }
     }
 }
